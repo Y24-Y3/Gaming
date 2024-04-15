e0aa5b6b8b0654bba9b59661cba2eb7c8aca58ab
Jardel - JD <mitchelljardel@gmail.com>
2024-04-15 14:26:24 -0400

    Game Development BaseFiles

---
+++ Game/GameApplication.java
@@ -0,0 +1,8 @@
+import javax.swing.JPanel;
+
+public class GameApplication extends JPanel{
+    public static void main(String[] args) {
+        GameWindow window = new GameWindow();
+    }
+    
+}
---
+++ Game/GameWindow.java
@@ -0,0 +1,8 @@
+import javax.swing.*;
+import java.util.*;
+import java.awt.*;
+
+
+public class GameWindow {
+    
+}
