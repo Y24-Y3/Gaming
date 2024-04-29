cba4a82e0bd19985eebb14b0d8b4dad776571157
joshuamvw <joshmalvalwil1@gmail.com>
2024-04-28 20:20:11 -0400

    Clean up + last class for Rahmoko added

--- GameWindow.java
+++ GameWindow.java
@@ -1,66 +1,273 @@
 import javax.swing.*;
 import java.awt.event.*;
+import java.util.HashSet;
 import java.awt.*;
 
 
-public class GameWindow extends JFrame implements ActionListener{
+public class GameWindow extends JFrame implements ActionListener, KeyListener, MouseListener{
+
+    //
+    //public HashSet<Integer> directions;
+    public boolean[] directions = {false, false, false, false};
+
+    // labels
+    private JLabel inventory, questItems, health, status, gamelevel;
     
-    private JLabel gameLevel, gameTextL;
-    private JTextField gameText;
+    // // text fields
+    // private JTextField statusTF, keyTF, mouseTF;
+    
+    // text areas
+    private JTextArea inventoryText, questItemsText, levelText;
 
-    private JButton start, quit;
+    // scroll panes
+    private JScrollPane inventoryScroll, questItemsScroll, healthScroll, statusScroll, levelScroll;
 
-    private JPanel mainPanel, buttonPanel;
+    // buttons
+    private JButton start, quit, pause, restart;
+    
+    // panels
+    private JPanel inventoryPanel, questItemsPanel, healthPanel, main, controls;
+    private GamePanel2 gamePanel;
     private GamePanel gp;
+
+    private CardLayout cardLayout;
+    private JPanel cardPanel;
+    
+    //private Keylisteners keylisteners;
     private Container c;
 
     @SuppressWarnings("unchecked")
-    public GameWindow() {
-        setTitle("Nomad RPG");
+    public GameWindow(){
+        setTitle("Nomad");
+        setSize(1200, 800);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLocation(100, 0);
+        setLocation(200, 0);
+
+        //directions = new HashSet<>();
+
+        // User Interface Components
+        inventory = new JLabel("Inventory");
+        questItems = new JLabel("Quest Items");
+        health = new JLabel("Health");
+        status = new JLabel("Status");
+        gamelevel = new JLabel("Game-Level");
+
+        // Panel Fields
+        inventoryPanel = new JPanel();
+        inventoryPanel.setLayout(new BoxLayout(inventoryPanel, BoxLayout.Y_AXIS));
+        inventoryPanel.add(inventory);
+        //inventoryPanel.add(inventoryScroll);
+
+
+        questItemsPanel = new JPanel();
+        questItemsPanel.setLayout(new BoxLayout(questItemsPanel, BoxLayout.Y_AXIS));
+        questItemsPanel.add(questItems);
+        //questItemsPanel.add(questItemsScroll);
+
+
+        healthPanel = new JPanel();
+        healthPanel.setLayout(new BoxLayout(healthPanel, BoxLayout.Y_AXIS));
+        healthPanel.add(health);
+
+        
+        controls = new JPanel();
+        controls.setBackground(Color.ORANGE);
+        main = new JPanel();
+        main.setBackground(Color.RED);
+
+        // Text Fields
+        inventoryText = new JTextArea(10, 20);
+        questItemsText = new JTextArea(10, 20);
+        levelText = new JTextArea(10, 20);
+
+        inventoryText.setEditable(false);
+		questItemsText.setEditable(false);
+		levelText.setEditable(false);
+
+        // buttons
+        start = new JButton("Start Game");
+        quit = new JButton("Quit");
+        pause = new JButton("Pause");
+        restart = new JButton("Restart");
+
+        start.addActionListener(this);
+        quit.addActionListener(this);
+        pause.addActionListener(this);
+        restart.addActionListener(this);
+        controls.add(start);
+        controls.add(quit);
+        controls.add(pause);
+        controls.add(restart);
+
+        // Scroll Panes
+        inventoryScroll = new JScrollPane(inventoryText);
+        questItemsScroll = new JScrollPane(questItemsText);
+        levelScroll = new JScrollPane(levelText);
+
+        // Initialize CardLayout and JPanel to hold different panels
+        cardLayout = new CardLayout();
+        cardPanel = new JPanel(cardLayout);
+
+        // Game Panel
+        gp = new GamePanel();
+        gp.setPreferredSize(new Dimension(1000, 700));
+        gamePanel = new GamePanel2();
+        gamePanel.setPreferredSize(new Dimension(1000, 700));
+
+        // Add GamePanel instances to cardPanel
+        cardPanel.add(gp, "gamePanel");
+        cardPanel.add(gamePanel, "gamePanel2");
+
+
+        
+        //Main Panel
+        main.add(cardPanel);
+        // Show the first panel (GamePanel) initially
+        cardLayout.show(cardPanel, "gamePanel");
+
+        //main.add(gamePanel);
+        main.add(controls);
+
+        // msn must respond to keyboard and mouse
+        gamePanel.addMouseListener(this);
+		main.addKeyListener(this);
+
+
+        // Container
+        c = getContentPane();
+        c.setLayout(new BorderLayout());
+        c.add(main, BorderLayout.CENTER);
+        //c.add(controls, BorderLayout.SOUTH);
+        setVisible(true);
         setResizable(false);
 
-        gameLevel = new JLabel("Level: 1");
-        gameLevel.setForeground(Color.WHITE);
-        gameTextL = new JLabel("Game Status: ");
-        gameText = new JTextField(20);
-        gameText.setEditable(false);
-
-        start = new JButton("Start");
-        start.addActionListener(this);
-        quit = new JButton("Quit");
-        quit.addActionListener(this);
-
-        JPanel controlPanel = new JPanel();
-        controlPanel.setLayout(new FlowLayout());
-        controlPanel.add(gameLevel);
-        controlPanel.add(gameTextL);
-        controlPanel.add(gameText);
-        controlPanel.add(start);
-        controlPanel.add(quit);
-
-        gp = new GamePanel();
-        //gp.setBackground(Color.BLUE);
-
-        setLayout(new BorderLayout());
-        add(gp, BorderLayout.CENTER);
-        add(controlPanel, BorderLayout.SOUTH);
-
-        pack();
-        setVisible(true);
     }
 
     public void actionPerformed(ActionEvent e){
-            gp.gameSetup();
-
+        
         if(e.getSource() == start){
-            gameText.setText("Game Started");
+            gp.gameSetup();
             gp.gameState = gp.getPlayState();
             gp.startGameThread();
+            // gamePanel.startGame();
         }
-        else if(e.getSource() == quit){
+        if(e.getSource() == quit){
             System.exit(0);
         }
+        if(e.getSource() == pause){
+            gp.endGameThread();
+            gamePanel.startGame();
+            cardLayout.show(cardPanel, "gamePanel2");
+            //gamePanel.pauseGame();
+
+            // if (command.equals("Pause Game"))
+			// 	pauseB.setText ("Resume");
+			// else
+			// 	pauseB.setText ("Pause Game");
+        }
+        if(e.getSource() == restart){
+            //gamePanel.restartGame();
+        }
+
+        main.requestFocus();
     }
+    // implement methods in KeyListener interface
+
+	public void keyPressed(KeyEvent e) {
+
+        int keyCode = e.getKeyCode();
+
+		if (keyCode == KeyEvent.VK_LEFT) {
+            directions[1] = true;
+			gamePanel.setDirections(directions);
+		}
+		//else
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            directions[2] = true;
+			gamePanel.setDirections(directions);
+		}
+        //else
+		if (e.getKeyCode() == KeyEvent.VK_UP) {
+            directions[3] = true;
+			gamePanel.setDirections(directions);
+		}
+
+        if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+            directions[0] = true;
+			gamePanel.setDirections(directions);
+		}
+        // else
+		// if (e.getKeyCode() == KeyEvent.VK_UP) {
+		// 	gamePanel.moveUp();
+		// }
+        // else
+		// if (e.getKeyCode() == KeyEvent.VK_DOWN) {
+		// 	gamePanel.moveDown();
+		// }
+		
+	}
+
+	public void keyReleased(KeyEvent e) {
+
+        int keyCode = e.getKeyCode();
+
+		if (keyCode == KeyEvent.VK_LEFT) {
+            directions[1] = false;
+			gamePanel.setDirections(directions);
+		}
+		//else
+		if (e.getKeyCode() == KeyEvent.VK_RIGHT) {
+            directions[2] = false;
+			gamePanel.setDirections(directions);
+		}
+        //else
+		if (e.getKeyCode() == KeyEvent.VK_UP) {
+            directions[3] = false;
+			gamePanel.setDirections(directions);
+		}
+        //else
+        if (e.getKeyCode() == KeyEvent.VK_SPACE) {
+            directions[0] = false;
+			gamePanel.setDirections(directions);
+		}
+
+	}
+
+	public void keyTyped(KeyEvent e) {
+
+	}
+
+
+	// implement methods in MouseListener interface
+
+	public void mouseClicked(MouseEvent e) {
+		int x = e.getX();
+		int y = e.getY();
+	}
+
+
+	public void mouseEntered(MouseEvent e) {
+	
+	}
+
+	public void mouseExited(MouseEvent e) {
+	
+	}
+
+	public void mousePressed(MouseEvent e) {
+	
+	}
+
+	public void mouseReleased(MouseEvent e) {
+	
+	}
+
+   
+    public void startLevel2(){
+        gp.endGameThread();
+        gamePanel.startGame();
+        cardLayout.show(cardPanel, "gamePanel2");
+        return;
+    }
+    
 }
---
+++ Rahmoko.java
@@ -0,0 +1,276 @@
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.awt.Point;
+import java.awt.geom.Rectangle2D;
+import java.awt.geom.Rectangle2D.Double;
+import java.util.Iterator;
+import java.util.LinkedList;
+
+public class Rahmoko implements Enemy{
+
+    private static final int XSIZE = 64;		// width of the image
+	private static final int YSIZE = 64;		// height of the image
+	//private static final int DX = 2;		// amount of pixels to move in one update
+	private static final int YPOS = 150;		// vertical position of the image
+    private static final int ATTACK_COOLDOWN_THRESHOLD = 8;
+    private static final int hurtDuration = 8;
+
+	private int x;
+	private int y;
+	private int dx;
+    private int originalX;
+
+	private Hunter player;
+	private TileMap2 map;
+
+	private Image spriteImage;			// image for sprite
+	private Image spriteLeftImageWalk;
+	private Image spriteRightImageWalk;
+    private Image spriteLeftImageIdle;
+    private Image spriteRightImageIdle;
+    private Image spriteLeftImageAttack;
+    private Image spriteRightImageAttack;
+    int lastDirection;
+    boolean gapClosed;
+    private boolean dead, attacking;
+	private int health;
+	private LinkedList bullets;
+    private int attackCooldown;
+    private boolean isHurting;
+    private Image spriteHurtImage;
+    private int hurtTimer;
+
+	//Graphics2D g2;
+
+	int time, timeChange;				// to control when the image is grayed
+	boolean originalImage, grayImage;
+
+	int count;
+	int count2;
+
+
+	public Rahmoko (TileMap2 map) {  
+        
+		// x = 4128;
+		// y = 270;
+		dx = 3;
+        originalX = x;
+
+		this.map = map;
+		this.player = map.getPlayer();
+
+		time = 0;				// range is 0 to 10
+		timeChange = 1;				// set to 1
+		originalImage = true;
+		grayImage = false;
+		count = 0;
+        lastDirection = 2;
+        gapClosed = false;
+        dead = false;
+        attacking = false;
+        attackCooldown = 0;
+		health = 10;
+        isHurting = false;
+        bullets = player.getBullets();
+		spriteLeftImageWalk = ImageManager2.loadImage("images2/stein/walkLeft60x100.gif");
+	    spriteRightImageWalk = ImageManager2.loadImage("images2/stein/walkRight60x100.gif");
+        spriteLeftImageIdle = ImageManager2.loadImage("images2/stein/idleLeft60x100.gif");
+        spriteRightImageIdle = ImageManager2.loadImage("images2/stein/idleRight60x100.gif");
+        spriteLeftImageAttack = ImageManager2.loadImage("images2/stein/attackLeft100x100.gif");
+        spriteRightImageAttack = ImageManager2.loadImage("images2/stein/attackRight100x100.gif");
+		
+		spriteImage = spriteLeftImageIdle;
+
+	}
+
+
+	public void draw (Graphics2D g2) {
+
+		g2.drawImage(spriteImage, x, y, XSIZE, YSIZE, null);
+
+	}
+
+
+	public boolean collidesWithPlayer () {
+		Rectangle2D.Double myRect = getBounds();
+		Rectangle2D.Double playerRect = player.getBounds();
+		
+		if (myRect.intersects(playerRect)) {
+			System.out.println ("Collision with player!");
+			return true;
+		}
+		else
+			return false;
+	}
+
+
+	public Rectangle2D.Double getBounds() {
+		return new Rectangle2D.Double (x, y+32, spriteImage.getWidth(null), spriteImage.getHeight(null));
+	}
+
+    public void update() {
+
+        if(health <= 0){
+			dead = true;
+            System.out.println("Stein killed");
+		}
+
+		if((player.getX() + 25) < (x + (spriteImage.getWidth(null)/2))){ //facing left
+			lastDirection = 1;
+		}else{
+			lastDirection = 2;
+		}
+
+		if (attackCooldown > 0) {
+            attackCooldown--;
+        }
+
+        double distance = Math.sqrt(Math.pow(player.getX() - getX(), 2) + Math.pow(player.getY() - getY(), 2));
+
+        if(distance < 80 && lastDirection == 2){ //attack range
+                spriteImage = spriteRightImageAttack;
+                attacking = true;
+        }else if(distance < 50 && lastDirection == 1){
+            spriteImage = spriteLeftImageAttack;
+            attacking = true;
+        }else if(distance < 400){// pursue range
+            attacking = false;
+            if(lastDirection == 1){
+                Point tilePos = collidesWithTile((x-dx), y);
+
+                if(tilePos == null){
+                    spriteImage = spriteLeftImageWalk;
+                    x = x - dx;
+                }else{
+                    spriteImage = spriteLeftImageIdle;
+                }
+                
+            }else{
+                Point tilePos = collidesWithTile((x+dx+ spriteImage.getWidth(null)), y);
+
+                if(tilePos == null){
+                    spriteImage = spriteRightImageWalk;
+                    x = x + dx;
+                }else{
+                    spriteImage = spriteRightImageIdle;
+                }
+            } 
+
+        }else{// do nothing
+            attacking = false;
+            if(lastDirection == 1){
+                spriteImage = spriteLeftImageIdle;
+            }else{
+                spriteImage = spriteRightImageIdle;
+            }
+
+        }
+
+        bullets = player.getBullets();
+        Iterator i2 = bullets.iterator();
+        while (i2.hasNext()) {
+            Bullet bullet = (Bullet)i2.next();
+			if(getBounds().intersects(bullet.getBounds())){
+				health = health - 1;
+                System.out.println("hit -1");
+				bullet.setFired();
+                isHurting = true;
+			}
+		}
+		
+        if(collidesWithPlayer() && attacking && attackCooldown <= 0){
+            player.takeDamage(1);
+            attackCooldown = ATTACK_COOLDOWN_THRESHOLD;
+        }  
+
+
+        spriteHurtImage = ImageManager2.tintImage(spriteImage, Color.RED);
+        if (isHurting) {
+            hurtTimer++;
+            if (hurtTimer >= hurtDuration) {
+                isHurting = false;
+                hurtTimer = 0; // Reset the timer
+            }
+        }
+
+    }
+
+
+	public int getX() {
+      		return x;
+   	}
+
+
+   	public void setX(int x) {
+      		this.x = x;
+   	}
+
+
+   	public int getY() {
+      		return y;
+   	}
+
+
+   	public void setY(int y) {
+      		this.y = y;
+   	}
+
+    public void setOriginalX(int x) {
+        this.originalX = x;
+    }
+
+
+   	public Image getImage() {
+        if (isHurting) {
+            return spriteHurtImage;
+        } else {
+            return spriteImage;
+        }
+   	}
+
+       public Rahmoko clone(){
+        return this;
+       }
+
+
+    @Override
+    public int getWidth() {
+        // return spriteImage.getWidth(null);
+        return spriteImage.getWidth(null);
+    }
+
+
+    @Override
+    public int getHeight() {
+        // return spriteImage.getHeight(null);
+        return spriteImage.getHeight(null);
+    }
+
+
+    @Override
+    public void wakeUp() {
+        return;
+    };
+
+    public Point collidesWithTile(int newX, int newY) {
+
+        //int playerWidth = spriteImage.getWidth(null);
+        int offsetY = map.getOffsetY();
+        int xTile = map.pixelsToTiles(newX);
+        int yTile = map.pixelsToTiles(newY - offsetY);
+
+        if (map.getTile(xTile, yTile) != null) {
+                Point tilePos = new Point (xTile, yTile);
+            return tilePos;
+        }
+        else {
+            return null;
+        }
+    }
+
+    public boolean dead(){
+		return dead;
+	}
+
+}
